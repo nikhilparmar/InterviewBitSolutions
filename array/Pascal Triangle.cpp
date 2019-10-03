b8a4d5a7349affbf5f3368cc9d8aa419d6ff266f
NIKHIL PARMAR <nikhil9.parmar@gmail.com>
2019-10-03 18:43:51 +0530

    Create Pascal Triangle.cpp

---
+++ array/Pascal Triangle.cpp
@@ -0,0 +1,22 @@
+vector<vector<int> > Solution::solve(int A) 
+{
+    vector<vector<int>> a;
+    int n=A;
+    if(n>0)
+    {
+        vector<int> x(1,1);
+        a.push_back(x);
+        for(int i=1;i<n;i++)
+        {
+            a.push_back(vector<int>(i+1,1));
+            for(int j=1;j<i;j++)
+            {
+                int x=i-1;
+                int y=j-1;
+                a[i][j]=a[x][j]+a[x][y];
+            }
+        }
+        return a;
+    }
+    return a;
+}
