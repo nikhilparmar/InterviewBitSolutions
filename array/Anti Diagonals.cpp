8cc6e0f44f63eecd322b647f38d28f82e53e6748
NIKHIL PARMAR <nikhil9.parmar@gmail.com>
2019-10-11 11:38:25 +0530

    Update Anti Diagonals.cpp

--- array/Anti Diagonals.cpp
+++ array/Anti Diagonals.cpp
@@ -37,3 +37,15 @@ vector<vector<int> > Solution::diagonal(vector<vector<int> > &A)
     
     return final;
 }
+// Editorial
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+    int n = A.size();
+    int N = 2*(n-1) + 1;//number of vectors in ans
+    vector<vector<int>> ans(N);
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0;j<n;j++)
+            ans[i+j].push_back(A[i][j]);//sum of index values in 2d array gives the index in ans
+    }
+    return ans;
+}
