b902835edf0a7971ffc510d8e0a507598316c121
NIKHIL PARMAR <nikhil9.parmar@gmail.com>
2019-10-20 07:29:13 +0530

    Create FizzBuzz.cpp

---
+++ Math/FizzBuzz.cpp
@@ -0,0 +1,28 @@
+vector<string> Solution::fizzBuzz(int A) 
+{
+    int n=A;
+    vector<string> vec;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            vec.push_back("FizzBuzz");
+        }
+        else
+        if(i%3==0)
+        {
+            vec.push_back("Fizz");
+        }
+        else
+        if(i%5==0)
+        {
+            vec.push_back("Buzz");
+        }
+        else
+        {
+            vec.push_back(to_string(i));
+        }
+    }
+    
+    return vec;
+}
